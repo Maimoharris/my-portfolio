aef18983ef41b6d4bdc4e28dd47f2605619f8521
Maimoharris <maimoharris111@gmail.com>
2025-09-19 13:39:28 +0300

    Debug Mode

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i4=swcq#zy)rf520g9v*$-q^x&tl2oy(1drdu=du56sl%qk@2$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
