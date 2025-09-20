d6107e3b8c362892e39069e0b5cee2af5a351deb
Maimoharris <maimoharris111@gmail.com>
2025-09-20 17:49:20 +0300

    Nexura Update

--- blog/models.py
+++ blog/models.py
@@ -65,3 +65,17 @@ class Post(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ['created_at']
+    
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
